b34c4e2fb66e1d27f39ed0818116ece2a55f7f8c
Deepika Sidda <siddadeepika@gmail.com>
2025-09-10 12:29:21 +0530

    Initial project commit

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-c2jsymoeo2go.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',        // replace if your master username is different
+    DB_PWD : 'Whiz-Auto',
+    DB_DATABASE : 'webappdb'
+});
